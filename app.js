e9806c8705f07ee83cc40a139273b005db3cbd4c
User <user@Users-MacBook-Pro.local>
2022-10-31 13:58:50 -0700

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+let answer = Math.floor(100* Math.random());
+let n1=0;
+let n2=99;
+
+while (true) {
+    let guess = prompt('Make your guess: (between ' +n1+' and '+n2 +')');
+    if(guess == answer) {
+        alert('you got it right');
+        break;
+    } else if(guess<=answer){
+        n1=guess;
+    } else if(guess>answer){
+        n2=guess;
+    }
+}
