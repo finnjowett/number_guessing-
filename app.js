8cc4a6f72055ea24e6674ff01032232a3a86c84f
User <user@Users-MacBook-Pro.local>
2022-10-31 14:03:01 -0700

    final

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let n2=99;
 
 while (true) {
     let guess = prompt('Make your guess: (between ' +n1+' and '+n2 +')');
+    if(guess<n1||guess>n2||isNaN(guess)){
+        alert('please make a valid input.');
+        continue;
+    }
     if(guess == answer) {
         alert('you got it right');
         break;
