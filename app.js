c255f9f7fd5fa4c71b700f6d5294cced490592d9
User <user@Users-MacBook-Pro.local>
2022-11-03 10:43:06 -0700

    sorting function

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let input = prompt('what is your name?');
+let title = document.querySelector('header h1');
+title.innerHTML = input + "'s to do list";
 let section = document.querySelector('section');
 let add = document.querySelector('form button');
 add.addEventListener('click',e => {
@@ -81,8 +84,9 @@ add.addEventListener('click',e => {
     section.appendChild(todo);
 })
 
-let myList = localStorage.getItem('list');
-if (myList !== null) {
+function loadData() {
+    let myList = localStorage.getItem('list');
+    if (myList !== null) {
     let myListArray = JSON.parse(myList);
     myListArray.forEach(item =>{
 
@@ -131,3 +135,107 @@ if (myList !== null) {
         section.appendChild(todo);
     })
 }
+}
+loadData();
+// let myList = localStorage.getItem('list');
+// if (myList !== null) {
+//     let myListArray = JSON.parse(myList);
+//     myListArray.forEach(item =>{
+
+//         // create a todo
+//         let todo = document.createElement('div');
+//         todo.classList.add('todo');
+//         let text = document.createElement('p');
+//         text.classList.add('todo-text');
+//         text.innerText = item.todoText;
+//         let date = document.createElement('p');
+//         date.classList.add('todo-time');
+//         date.innerText = item.date;
+//         todo.appendChild(text);
+//         todo.appendChild(date);
+//         let completeBtton = document.createElement('button');
+//         completeBtton.classList.add('complete');
+//         completeBtton.innerHTML = '<i class="fa-solid fa-check"></i>'
+//         completeBtton.addEventListener('click',e=>{
+//             let todoItem = e.target.parentElement;
+//             todoItem.classList.toggle('done')
+//         })
+//         let trashButton = document.createElement('button');
+//         trashButton.classList.add('trash');
+//         trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+//         trashButton.addEventListener('click',e=>{
+//             let todoItem = e.target.parentElement;
+//             todoItem.style.animation = 'scaleDown 1s forwards';
+//             todoItem.addEventListener('animationend',()=>{
+//                 todoItem.remove();
+//                 // remove from localStorage
+//                 let text = todoItem.children[0].innerText;
+//                 let myListArray = JSON.parse(localStorage.getItem('list'));
+//                 myListArray.forEach((item,index)=>{
+//                     if (item.todoText == text) {
+//                         myListArray.splice(index,1);
+//                         localStorage.setItem('list',JSON.stringify(myListArray))
+//                     }
+//                     localStorage.removeItem(todoText);
+//                 })
+//             })
+//         })
+//         todo.appendChild(completeBtton);
+//         todo.appendChild(trashButton);
+
+//         section.appendChild(todo);
+//     })
+// }
+
+function merge(arr1,arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    let date1 = new Date(arr1[i].date).getTime();  
+    let date2 = new Date(arr2[j].date).getTime();
+    while (i< arr1.length && j<arr2.length) {
+        if (Number(date1) > Number(date2)){
+          result.push(arr2[j]);
+          j++; 
+        } else{
+            result.push(arr1[i]);
+            i++;
+        }
+    }
+    while (i<arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+    while (j<arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+    return result;
+}
+
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    } else {
+        let middle = Math.floor(arr.length/2);
+        let left = arr.slice(0,middle);
+        let right = arr.slice(middle,arr.length);
+        return merge(mergeSort(left),mergeSort(right));
+    }
+}
+let sort = document.querySelector('div.sort');
+sort.addEventListener('click',()=>{
+    // sort data
+    let sortedArray = JSON.parse(localStorage.getItem('list')).sort((a,b)=>new Date(a.date).getTime()-new Date(b.date).getTime());
+    localStorage.setItem('list',JSON.stringify(sortedArray));
+
+    //remove data
+    let len = section.children.length;
+    for (let i=0; i<len;i++) {
+        section.children[0].remove();
+    }
+    
+    // load data
+    loadData();
+})
